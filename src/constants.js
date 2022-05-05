e0a5cc5ff68d4ffd302670b0e11f1bb31c236c4b
DD155 <31744748+DD155@users.noreply.github.com>
2022-05-05 15:07:42 -0400

    deployment stuff

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,3 @@ export const SERVER_PATH = "https://cine-track.herokuapp.com/"
 
 export const MOVIE_IMG_PATH = "/movies/"
 export const SHOW_IMG_PATH = "/shows/"
-
-
-export const HOME_SLIDE1_MSG = "CineTrack is a movie and TV show curator website. This website lets users check out a catalog of movies and shows and be able to keep track of what they want to watch or have watched already. It also has features such as leaving reviews on a certain movie or show and being able to see the main actors and actresses for each selection."
