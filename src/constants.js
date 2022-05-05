6fc5096f963c01cf4bbea439104fadb7d7f4bf12
DD155 <31744748+DD155@users.noreply.github.com>
2022-05-05 15:59:04 -0400

    a

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const LOCAL_SERVER_PATH = "http://localhost:3001/"
-export const SERVER_PATH = "https://cinetrack-server.herokuapp.com/"
+export const SERVER_PATH = 'cine-track.herokuapp.com/api/'
 
 export const MOVIE_IMG_PATH = "/movies/"
 export const SHOW_IMG_PATH = "/shows/"
